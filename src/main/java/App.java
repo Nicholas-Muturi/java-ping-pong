35eac28bf62fb5136f9340e892969d94436fe73c
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-07 21:29:42 +0300

    Test Jenkins Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("I'm a ping-pong application!");
+        System.out.println("Enter a number:");
+        int userInput = input.nextInt();
+
+        PingPong pingPong = new PingPong();
+        ArrayList<Object> pingPongResult = pingPong.runPingPong(userInput);
+        System.out.println(pingPongResult);
+        input.close();
+    }
+}
