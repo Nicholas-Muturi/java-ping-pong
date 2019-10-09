9b3b63eb35ac345cd030f46b8d106a9ad4c9eb31
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-09 12:29:22 +0300

    [junk] Added hyphens

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,9 @@ public class App {
         Scanner input = new Scanner(System.in);
 
         System.out.println("I'm a ping-pong application!");
+        System.out.println("----------------------------");
         System.out.println("Enter a number:");
         int userInput = input.nextInt();
-
         PingPong pingPong = new PingPong();
         ArrayList<Object> pingPongResult = pingPong.runPingPong(userInput);
         System.out.println(pingPongResult);
